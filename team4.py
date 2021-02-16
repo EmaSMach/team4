32a21443bef8cd055481bdb0f23619c3f388cad6
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-02-16 00:10:05 -0300

    agregado estructura básica del programa.

---
+++ team4.py
@@ -0,0 +1,28 @@
+def pedir_palabra() -> str:
+    # se puede agregar validación
+    """Pide una palabras, y las retorna."""
+    return
+
+
+def separar_silabas(palabra: str) -> list:
+    """Separa la palabra dada en sílabas, retorna la lista de sílabas."""
+    return
+
+
+def generar_palabra(palabras_semilla: list) -> str:
+    """Genera unas palabras a partir de las palabras que se le pasen."""
+    return
+
+
+def mostrar_palabras(palabras_generadas: list, palabras_semilla: list):
+    """Muestra las palabras base, y las generadas."""
+    return
+
+
+def main():
+    # Logica principal del programa
+    return
+
+
+if __name__ == '__main__':
+    main()
