4890c087d200fbe48757788a1d989845605799d6
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-02-22 00:27:07 -0300

    pequeña corrección, archivo no estaba guardado al comitear

--- team4.py
+++ team4.py
@@ -1,4 +1,12 @@
-def es_palabra_válida(palabra: str, min_length: int) -> bool:
+#!/usr/bin/env python3
+import random
+from os import system
+
+
+system("cls")
+
+
+def es_palabra_valida(palabra: str, min_length: int) -> bool:
     """
     Verifica que la palabra dada sea válida, y que tenga el largo mínimo indicado.
     """
@@ -11,19 +19,17 @@ def es_palabra_válida(palabra: str, min_length: int) -> bool:
         raise TypeError("'palabra' debe ser un string")
 
 
-def pedir_palabra(min_length=1) -> str:
+def pedir_palabra(min_length: int = 2) -> str:
     """
     Pide una palabra, y la retorna.
     Sigue pidiendo hasta que se ingrese una palabra válida.
     """
     while True:
-        # esto puede agregarse como un parámetro
-        err_msg = "Ingrese una palabra válida"
         palabra = input("Ingrese palabra: ").strip()
-        if es_palabra_válida(palabra, min_length):
+        if es_palabra_valida(palabra, min_length):
             return palabra
         else:
-            print(err_msg)
+            print("Ingrese una palabra válida")
 
 
 def separar_silabas(palabra: str) -> list:
@@ -33,7 +39,23 @@ def separar_silabas(palabra: str) -> list:
 
 def generar_palabra(silabas_semilla: list) -> str:
     """Genera una palabra a partir de la lista de sílabas."""
-    return
+    # ejemplo [["","","",""],["",""]]
+    salida =''
+    aux_rnd = random.randrange(2)
+    if aux_rnd == 0:
+    # tomando una sola silaba por cada palabra
+        for x in silabas_semilla:
+            r = random.randrange(len(x)-1)
+            salida += x[r]
+    elif aux_rnd ==1:
+        # toma 2 silabas de la primera y 2 silaba de cada siguente
+         
+        for x in silabas_semilla:
+            r = random.randrange(len(x)-1)
+            salida += x[r]
+            r = random.randrange(len(x)-1)
+            salida += x[r]
+    return salida
 
 
 def mostrar_palabras(palabras_generadas: list, palabras_semilla: list):
