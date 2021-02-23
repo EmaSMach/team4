0d5723a6f1aad9c13beea51a4ff5f3515b245634
4ndroid3 <andijaimo@gmail.com>
2021-02-22 23:10:55 -0300

    se vuelve a imprimir por consola el resultado

--- team4.py
+++ team4.py
@@ -1,5 +1,6 @@
 # Imports
 from tkinter import *
+from separasilabas import silabizer
 
 def pedir_palabra() -> str:
     # se puede agregar validación
@@ -11,71 +12,29 @@ def separar_silabas(palabra: str) -> list:
     """Separa la palabra dada en sílabas, retorna la lista de sílabas."""
     return
 
-
 def generar_palabra(silabas_semilla: list) -> str:
     """Genera una palabra a partir de la lista de sílabas."""
     return
 
 
-def mostrar_palabras(palabras_semilla: list, palabra_generada: str, main_window):
+def mostrar_palabras(palabras_semilla: list, palabra_generada: str):
     """Muestra las palabras semilla, y la palabra generada."""
-
-    font = ('Verdana', 21, 'bold')
-
-    # Frame que muestra las palabras ingresadas
-    frame_mostrar_palabras = LabelFrame(
-        main_window, 
-        text = 'MOSTRAR LAS PALABRAS CARGADAS:',
-        font = font
+    print(
+        "Se ingresaron {} palabras semillas: {}".format(
+            len(palabras_semilla), 
+            (', ').join(palabras_semilla).upper())
     )
 
-    frame_mostrar_palabras.grid(row = 0, column = 0, sticky = 'WENS')
-
-    label_palabras_mostradas = Label(
-        frame_mostrar_palabras, 
-        text = (', '.join(palabras_semilla)).upper(),
-        font = font
+    print(
+        'La palabra creada es: {}'.format(
+            palabra_generada.upper()
+        )
     )
 
-    label_palabras_mostradas.grid(row = 0, column = 0, sticky = 'WENS')
-
-    # Frame que muestra la palabra creada.
-    frame_palabra_creada = LabelFrame(
-        main_window, 
-        text = 'MOSTRAR LA PALABRA CREADA:',
-        font = font
-    )
-
-    frame_palabra_creada.grid(row = 1, column = 0, sticky = 'WENS')
-
-    label_palabras_mostradas = Label(
-        frame_palabra_creada, 
-        text = palabra_generada.upper(),
-        font = font
-    )
-
-    label_palabras_mostradas.grid(row = 0, column = 0, sticky = 'WENS')
-
-    # Botón para cerrar la ventana
-    boton_cerrar = Button(
-        main_window, 
-        text = 'Cerrar',
-        command = main_window.destroy,
-        font = font
-    )
-
-    boton_cerrar.grid(row = 2, column = 0, sticky = 'NS')
- 
-
-def main(main_window):
+def main():
     # Logica principal del programa
-    palabras_semilla = ['ñandu', 'rio', 'bicicleta']
-    palabra_generada = 'ñaocleta'
-    mostrar_palabras(palabras_semilla, palabra_generada, main_window)
     pass
     
 if __name__ == '__main__':
-    main_window = Tk()
-    main_window.title("Palabreitor")
-    main(main_window)
-    main_window.mainloop()
+    main()
+    
